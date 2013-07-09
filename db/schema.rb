b96aee637cb0bf3591f335ef4040d5ebd9a21d97
zhangmeteor <438114643@qq.com>
2013-07-09 23:59:59 +0800

    mebay finished

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130704181708) do
+ActiveRecord::Schema.define(version: 20081127162635) do
 
   create_table "ads", force: true do |t|
     t.string   "name"
